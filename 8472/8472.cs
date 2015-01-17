58649245baef6267f8d6728eb322c7135fcf3dc1
blackhat-blade <blade+gh@sanemail.de>
2015-01-17 12:10:38 +0100

    Broken version. There are strange things done.. Throws a key already in dictionary if the definition(!) of JobInventarizer is  in the code. Dunno how a sole definition can cause a runtime error.

--- 8472/8472.cs
+++ 8472/8472.cs
@@ -4,6 +4,7 @@
 #use Multiplexer
 #use MultiplexHelp
 #use MultiplexInterfaceRotor
+#use Multiplexed
  
 string[] 	cargoNames	= { "8472 Cargo", "8472 Drill" }; 
 string 		overallName 	= "8472 Cockpit"; 
@@ -11,8 +12,34 @@ string 		overallName 	= "8472 Cockpit";
 string 		dumpName 	= "8472 Cockpit"; 
 
 Dumper 		dumper;
-Inventarizer 	foobar; //if you need to know what items we have, ask foobar
 CargoLeveler 	leveler; 
+/*
+public class JobInventarizer : Multiplexed
+{
+	Inventarizer o;
+	Dumper	     dumper;
+	
+	public JobInventarizer(Dumper d, string[] names, IMyGridTerminalSystem gts)
+	{
+		name        = "Cargo Items";
+		description = "Show amount of each Item in Cargo";
+		dumper 	    = d;
+		o           = new Inventarizer(names, gts, d);
+	}
+
+	public override void deactivate()
+	{
+		dumper.clear();
+	}
+
+	public override void run()
+	{
+		dumper.clear(); 
+		o.calculateCargoAmount();
+		o.dumpCargoAmount(); 
+	}
+}
+*/
 
 MultiplexInterfaceRotor ui;
 Multiplexer		m;
@@ -22,19 +49,21 @@ bool setup = false;
  
 void init()
 {
+	
 	if (setup)
 	{
 		return;
 	}
 	setup = true;
 	dumper  = new Dumper(dumpName, GridTerminalSystem);
-	foobar  = new Inventarizer(cargoNames, GridTerminalSystem, dumper);
         leveler = new CargoLeveler(overallName, cargoNames, GridTerminalSystem);
 	
 	ui = new MultiplexInterfaceRotor(GridTerminalSystem);
 	m  = new Multiplexer(ui);
 	h  = new MultiplexHelp(dumper, m);
+	
 	m.addJob(h);
+	//m.addJob(new JobInventarizer(dumper, cargoNames, GridTerminalSystem));
 
 }
 
@@ -50,14 +79,5 @@ void displayLevel()
 {
 	//that much?
 	leveler.displayLevel();
-}
-
-// blade -
-
-void displayCargo() 
-{ 
-	dumper.clear(); 
-	foobar.calculateCargoAmount();
-	foobar.dumpCargoAmount(); 
 }
 
