0390470868923a1cd085b52fa1d576d556c02d11
blade <blade@cn1.sanemail.de>
2015-09-21 04:29:56 +0200

    added airlockdoor

---
+++ airlock_door/AirLockDoor.cs
@@ -0,0 +1,20 @@
+
+public class AirLockDoor
+{
+	IMyGridTerminalSystem	GTS;
+	IMyTerminalBlock	DOOR_1;
+	IMyTerminalBlock	DOOR_2;
+	
+
+	public AirLockDoor (string n1, string n2 IMyGridTerminalSystem gts)
+	{
+		GTS       = gts;
+	}
+
+	IMyTerminalBlock by_name(string n)
+	{
+		
+	}
+
+	void 
+}
